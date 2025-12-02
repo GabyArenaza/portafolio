cb1ea55c91396186e2a78ba6dfe92dcf5f67316b
GabyArenaza <arenazag@gmail.com>
2025-12-02 09:28:20 -0600

    Add files via upload

---
+++ anidamiento.py
@@ -0,0 +1,10 @@
+edad = 30
+nacionalidad = "chileno"
+
+if edad >= 18:
+    if nacionalidad == "chileno":
+        print ("Puedes votar en Chile")
+    else:    
+        print("Necesitas la nacionalidad Chilena para votar en Chile")
+else:
+    print("Necesitas ser mayor de edad para votar")        
---
+++ asignacion_multiple_enunalinea.py
@@ -0,0 +1,10 @@
+
+a, b, c = 1, 2, 3
+print(a, b, c)  # Output: 1 2 3
+
+a,b,c = 1,2,3
+
+nombre,apellido,edad = "Juan", "Pérez", 30
+
+print("valores a", a,b,c)
+print("valores nombre, apellido, edad:", nombre, apellido, edad)
